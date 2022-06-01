dfd54b8c14b0f0ce3d08b38f450c19ccaf515a85
gideonler <gideonler@gmail.com>
2022-06-01 23:43:38 +0800

    Removed generator

--- lib/main.dart
+++ lib/main.dart
@@ -55,42 +55,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Startup name generator'),
         ),
-        body: const Center(
-          child: RandomWords(),
-        ),
+      
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      },
-    );
-  }
-}
